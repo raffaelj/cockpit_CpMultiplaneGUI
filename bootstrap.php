11f3a61310232ed834724fa81a0a40c2e0f6ee7a
Raffael <github@raffaeljesche.de>
2020-07-15 10:53:18 +0200

    added auto config for UniqueSlugs addon

--- bootstrap.php
+++ bootstrap.php
@@ -203,6 +203,37 @@ $this->module('cpmultiplanegui')->extend([
 
 ]);
 
+// pass config to UniqueSlugs addon if not already present
+$this->on('cockpit.bootstrap', function() {
+
+    if (!isset($this['modules']['uniqueslugs'])) return;
+
+    $config = $this->module('cpmultiplanegui')->getConfig();
+
+    if (empty($config['use']['collections'])) return;
+
+    $isMultilingual = $config['isMultilingual'] ?? false;
+
+    $uConfig = $this->retrieve('unique_slugs');
+
+    foreach($config['use']['collections'] as $col) {
+
+        $fieldName = 'title';
+
+        if (!isset($uConfig['collections'][$col])) {
+            $uConfig['collections'][$col] = $fieldName;
+        } else {
+            $fieldName = $uConfig['collections'][$col];
+        }
+
+        if ($isMultilingual && !isset($uConfig['localize'][$col])) {
+            $uConfig['localize'][$col] = $fieldName;
+        }
+    }
+
+    $this->set('unique_slugs', $uConfig);
+
+}, 100);
 
 // ACL
 $app('acl')->addResource('cpmultiplanegui', ['create', 'delete', 'manage']);
