3414710e5b4284708965aac430ba8247b750a4d0
Raffael <github@raffaeljesche.de>
2020-07-19 19:34:53 +0200

    removed unused config in singletons settings

--- views/partials/singletons.settings.aside.php
+++ views/partials/singletons.settings.aside.php
@@ -4,9 +4,6 @@
         <img src="@url('cpmultiplanegui:icon.svg')" width="30" height="30" alt="@lang('CpMultiplane')" />
     </div>
     <div class="uk-margin-small">
-        <field-boolean bind="singleton.multiplane.use" title="@lang('Use this singleton with CpMultiplane')" label="@lang('Use with CpMultiplane')"></field-boolean>
-    </div>
-    <div class="uk-margin-small">
         <field-boolean bind="singleton.multiplane.gui_in_header" title="@lang('Show in header menu')" label="@lang('Show in header menu')"></field-boolean>
     </div>
 </div>
