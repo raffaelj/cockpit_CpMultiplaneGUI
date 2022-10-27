8d25dab21601f235b999fda4770f98408a5b927b
Raffael <github@raffaeljesche.de>
2022-10-27 11:44:18 +0200

    added language option to form.settings.aside, improved UI

--- views/partials/forms.settings.aside.php
+++ views/partials/forms.settings.aside.php
@@ -1,3 +1,18 @@
-<div class="uk-margin">
-    <field-boolean bind="form.multiplane.gui_in_header" label="@lang('Show in header menu')"></field-boolean>
+<div class="uk-margin uk-panel-box uk-panel-card uk-panel-box-secondary">
+    <div class="uk-panel-box-header">
+        <img class="uk-margin-small-right" src="@url('cpmultiplanegui:icon.svg')" alt="icon" data-uk-svg width="20px" height="20px" />
+        CpMultiplane
+    </div>
+    <div class="uk-margin">
+        <label class="uk-text-small">@lang('Language')</label>
+        <select class="uk-width-1-1" bind="form.multiplane.language" bind-event="input">
+            <option value=""></option>
+            @foreach($app['languages'] as $k => $v)
+            <option value="{{ $k != 'default' ? $k : $app('i18n')->locale }}">{{ $v }}</option>
+            @endforeach
+        </select>
+    </div>
+    <div class="uk-margin">
+        <field-boolean bind="form.multiplane.gui_in_header" label="@lang('Show in header menu')"></field-boolean>
+    </div>
 </div>
