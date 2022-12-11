f0acfeb0b90ee99adea3423528e06aba20a00d9b
Raffael <github@raffaeljesche.de>
2022-12-11 10:16:51 +0100

    cleanup

--- views/partials/settings.php
+++ views/partials/settings.php
@@ -1,11 +1,11 @@
 
 <div>
-    <div class="uk-panel uk-panel-space uk-panel-box uk-panel-card">
-        <img src="@url('cpmultiplanegui:icon.svg')" width="50" height="50" alt="@lang('Multiplane')" />
+    <div class="uk-panel uk-panel-space uk-panel-box uk-panel-card uk-panel-card-hover">
+        <img src="@url('cpmultiplanegui:icon.svg')" width="50" height="50" alt="" />
 
         <div class="uk-text-truncate uk-margin">
-            @lang('Multiplane')
+            Multiplane
         </div>
-        <a class="uk-position-cover" aria-label="@lang('Multiplane')" href="@route('/multiplane')"></a>
+        <a class="uk-position-cover" aria-label="Multiplane" href="@route('/multiplane')"></a>
     </div>
 </div>
