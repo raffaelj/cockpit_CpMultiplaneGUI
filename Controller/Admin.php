2d82d99e7825c760494a9343c6c25846892df314
Raffael <github@raffaeljesche.de>
2022-12-29 19:10:57 +0100

    added `/multiplane/getBranding` route, cleanup

--- Controller/Admin.php
+++ Controller/Admin.php
@@ -16,7 +16,7 @@ class Admin extends \Cockpit\AuthController {
 
         return $this->render('cpmultiplanegui:views/index.php', compact('profiles', 'currentProfile'));
 
-    } // end of index()
+    }
 
     public function profile($name = null) {
 
@@ -77,7 +77,7 @@ class Admin extends \Cockpit\AuthController {
             'themes'
         ));
 
-    } // end of profile()
+    }
 
     public function save_profile($name) {
 
@@ -93,9 +93,11 @@ class Admin extends \Cockpit\AuthController {
 
         return $this->app->module('cpmultiplanegui')->saveProfile($name, $profile);
 
-    } // end of save_profile()
+    }
 
-    // delete multiplane key in cockpit/options from old ui
+    /**
+     * delete multiplane key in cockpit/options from old ui
+     */
     public function clean_database() {
 
         if (!$this->module('cockpit')->hasaccess('cpmultiplanegui', 'manage')) {
@@ -106,7 +108,7 @@ class Admin extends \Cockpit\AuthController {
 
         return compact('return');
 
-    } // end of clean_database()
+    }
 
     public function get_multiplane_config() {
 
@@ -132,7 +134,7 @@ class Admin extends \Cockpit\AuthController {
             return ['error' => $e->getMessage()];
         }
 
-    } // end of get_multiplane_config()
+    }
 
     public function edit_forms_in_use() {
 
@@ -165,6 +167,77 @@ class Admin extends \Cockpit\AuthController {
         $result = $this->app->module('cpmultiplanegui')->saveProfile($profile['name'], $profile);
 
         return $result ? ['success' => true] : ['success' => false];
+
+    }
+
+    /**
+     * Get site branding config with fallback values from (localized) seo field
+     * in site singleton or system defaults.
+     *
+     * Use this method in the seo field options with
+     * `"optsFromRequest": "/multiplane/getBranding"`
+     *
+     * Fallback examples:
+     *
+     * branding: singleton/seo/config/branding || singleton/site_name || app.name
+     *
+     * branding_de: singleton/seo_de/config/branding || singleton/seo/config/branding
+     *              || singleton/site_name_de  || singleton/site_name || app.name
+     */
+    public function getBranding() {
+
+        $data = [];
+        $site = [];
+
+        $config = $this->app->module('cpmultiplanegui')->getConfig();
+
+        if (!empty($config['siteSingleton'])) {
+            $site = $this->app->module('singletons')->getData($config['siteSingleton']) ?? [];
+        }
+
+        $seoName = $this->app->module('cpmultiplanegui')->fieldNames['seo'];
+        $seo = isset($site[$seoName]) && \is_array($site[$seoName]) ? $site[$seoName] : [];
+
+        $site_name = $site['site_name'] ?? $this->app['app.name'];
+
+        $defaultBranding = !empty($seo['config']['branding'])
+                           ? $seo['config']['branding'] : $site_name;
+
+        $defaultSpacer   = !empty($seo['config']['spacer'])
+                           ? $seo['config']['spacer'] : ' - ';
+
+        $data['branding']     = $defaultBranding;
+        $data['spacer']       = $defaultSpacer;
+
+        // TODO:
+        // $data['hideBranding'] = $seo['config']['hideBranding'] ?? false;
+
+        $appLanguages = $this->app['languages'] ?? [];
+        foreach ($appLanguages as $code => $label) {
+
+            if ($code == 'default') continue;
+
+            $seoName = "{$seoName}_{$code}";
+
+            if (!empty($site["site_name_{$code}"])) {
+                $site_name = $site["site_name_{$code}"];
+            }
+            $seo = isset($site[$seoName]) && \is_array($site[$seoName]) ? $site[$seoName] : [];
+
+            $branding = !empty($seo['config']['branding'])
+                        ? $seo['config']['branding'] : $site_name;
+
+            $spacer   = !empty($seo['config']['spacer'])
+                        ? $seo['config']['spacer'] : $defaultSpacer;
+
+            $data["branding_{$code}"]     = $branding;
+            $data["spacer_{$code}"]       = $spacer;
+
+            // TODO:
+            // $data["hideBranding_{$code}"] = $seo['config']['hideBranding'] ?? false;;
+        }
+
+        return $data;
 
     }
 
